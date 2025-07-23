a9e0c9cce8e1cfa037e90151f5a8ea00eac218a1
Isabella Firmino <isabella.mfbr@gmail.com>
2025-07-23 13:53:53 -0300

    add loader

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="w-[250px] h-screen fixed left-0 top-0 bg-[#202020] flex flex-col justify-between py-4 text-[#f7f7f7] p-6 space-y-6 shadow-md">
-     <div>
+    <div>
       <div class="flex flex-col mb-14 mt-3">
         <div class="flex items-center gap-2">
           <i class="bx bxs-cat text-2xl"></i>
@@ -8,7 +8,6 @@
         </div>
         <p class="text-sm font-semibold text-[#f7f7f7] uppercase tracking-widest mb-4 ml-8">Menu</p>
       </div>
-
 
       <ul class="space-y-11">
         <li class="flex items-center gap-3 cursor-pointer hover:text-[#525392]">
@@ -30,15 +29,15 @@
       <!-- Perfil -->
       <div class="flex items-center gap-3 mb-6">
         <img
-          src=""
+          src="../assets/image.png"
           alt="avatar"
           class="w-[30px] h-[30px] rounded-full object-cover"
         />
-        <span class="text-sm">Nome</span>
+        <span class="text-sm">{{ userName }}</span>
       </div>
 
       <!-- Logout -->
-      <div class="flex items-center gap-3 bg-[#525392] p-3 rounded-lg cursor-pointer hover:opacity-90">
+      <div class="flex items-center gap-3 bg-[#525392] p-3 rounded-lg cursor-pointer hover:opacity-90" @click="logout">
         <i class="bx bx-log-out text-lg"></i>
         <span class="text-sm font-semibold">Logout</span>
       </div>
@@ -47,14 +46,26 @@
 </template>
 
 <script lang="ts" setup>
+import { onMounted, ref } from 'vue'
 import { useAuthStore } from '../stores/auth'
 import { useRouter } from 'vue-router'
+import api from '../services/api'
 
 const router = useRouter()
 const auth = useAuthStore()
+const userName = ref('User')
 
 const logout = async () => {
   await auth.logout()
   router.push('/login')
 }
+
+onMounted(async () => {
+  try {
+    const res = await api.get('/api/user')
+    userName.value = res.data.name || 'User'
+  } catch (err) {
+    console.warn('Error:', err)
+  }
+})
 </script>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,40 +1,68 @@
 <template>
-  <div class="flex items-start justify-between">
+  <div class="flex w-[78vw] items-start justify-between">
     <div class="p-6 mb-7">
-      <h2 class="text-3xl text-f7f7f7">Welcome, </h2>
+      <h2 class="text-3xl text-f7f7f7">Welcome, {{ userName }}</h2>
       <p class="text-md text-f7f7f7 mt-2" style="font-weight: 300;">Manage your tasks</p>
     </div>
     <SearchInput v-model="search" placeholder="Search..." />
   </div>
-  <div class="p-6">
-    <h2 class="text-2xl text-f7f7f7 mb-7">Tasks</h2>
-    <HorizontalSlider :items="tasks" :cardComponent="TaskCard" />
 
-    <h2 class="text-2xl text-f7f7f7 mb-7 mt-15">Reminders</h2>
-    <HorizontalSlider :items="reminders" :cardComponent="ReminderCard" />
+  <div v-if="isLoading" class="p-6">
+    <FullPageLoader />
+  </div>
 
-    <h2 class="text-2xl text-f7f7f7 mb-7 mt-15">Boards</h2>
-    <HorizontalSlider :items="boards" :cardComponent="BoardCard" />
+  <div v-else class="p-6">
+    <div v-if="isSearching">
+      <h2 class="text-2xl text-f7f7f7 mb-7">Search Results</h2>
+      <div v-if="searchResults.length">
+        <div class="flex gap-7">
+          <TaskCard v-for="task in searchResults" :key="task.id" :item="task" />
+        </div>
+      </div>
+      <div v-else class="text-center text-f7f7f7 mt-6">
+        Not found.
+      </div>
+    </div>
 
+    <div v-else>
+      <h2 class="text-2xl text-f7f7f7 mb-7">Tasks</h2>
+      <HorizontalSlider :items="tasks" :cardComponent="TaskCard" />
+
+      <h2 class="text-2xl text-f7f7f7 mb-7 mt-15">Reminders</h2>
+      <HorizontalSlider :items="reminders" :cardComponent="ReminderCard" />
+
+      <h2 class="text-2xl text-f7f7f7 mb-7 mt-15">Boards</h2>
+      <HorizontalSlider :items="boards" :cardComponent="BoardCard" />
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { ref, onMounted } from 'vue'
+import { ref, onMounted, watch } from 'vue'
 import api from '../services/api'
 import HorizontalSlider from '../components/HorizontalSlider.vue'
 import BoardCard from '../components/BoardCard.vue'
 import TaskCard from '../components/TaskCard.vue'
 import ReminderCard from '../components/ReminderCard.vue'
 import SearchInput from '../components/SearchInput.vue'
+import FullPageLoader from '../components/FullPageLoader.vue'
 
 const tasks = ref([])
 const boards = ref([])
 const reminders = ref([])
+
 const search = ref('')
+const isSearching = ref(false)
+const isLoading = ref(false)
+const searchResults = ref([])
+const userName = ref('User')
 
 onMounted(async () => {
+  isLoading.value = true
+
   try {
+    
+
     const [tasksRes, boardsRes, remindersRes] = await Promise.all([
       api.get('/api/tasks'),
       api.get('/api/boards'),
@@ -44,9 +72,35 @@ onMounted(async () => {
     tasks.value = tasksRes.data.data
     boards.value = boardsRes.data.data
     reminders.value = remindersRes.data
-    
+
+    const userRes = await api.get('/api/user')
+    userName.value = userRes.data.name || 'User'
   } catch (err) {
-    console.error('Error', err)
+    console.error('Error loading dashboard data', err)
+  } finally {
+    isLoading.value = false 
+  }
+})
+
+watch(search, async (value) => {
+  if (!value) {
+    isSearching.value = false
+    return
+  }
+
+  isSearching.value = true
+  isLoading.value = true
+
+  try {
+    const res = await api.get('/api/tasks', {
+      params: { search: value }
+    })
+    searchResults.value = res.data.data || []
+  } catch (err) {
+    console.error('Error during search', err)
+    searchResults.value = []
+  } finally {
+    isLoading.value = false
   }
 })
 </script>
